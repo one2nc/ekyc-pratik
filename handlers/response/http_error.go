255a28caaa3b1f169576f8a527d44a8c59b5766d
pratikjethe <pratikjethe93@gmail.com>
2023-07-24 11:57:51 +0530

    removed logs

--- handlers/response/http_error.go
+++ handlers/response/http_error.go
@@ -2,13 +2,11 @@ package response
 
 import (
 	"errors"
-	"fmt"
 	service "go-ekyc/services"
 	"net/http"
 )
 
 func GetHttpStatusAndError(err error) (int, error) {
-	fmt.Println("heloo")
 	switch err {
 	case service.ErrCustomerNotFound, service.ErrImageNotFound, service.ErrPlanNotFound:
 		return http.StatusNotFound, err
@@ -16,11 +14,8 @@ func GetHttpStatusAndError(err error) (int, error) {
 		return http.StatusConflict, err
 	case service.ErrInvalidImageType:
 		return http.StatusUnprocessableEntity, err
-	case service.ErrUnknown:
-		fallthrough
 	default:
 
-		fmt.Println("hello")
 		return http.StatusInternalServerError, errors.New("something went wrong")
 	}
 }
